3a43b756eb462fae8e65762820242edd5fab5441
Akarsh Prabhu K <akak@microsoft.com>
2021-05-11 11:02:06 +0530

    Add project files.

---
+++ CowinAPI/CowinEntity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowinAPI
+{
+
+    public class SessionsEntity
+    {
+        public Session[] sessions { get; set; }
+    }
+
+
+    public class SessionsCalendarEntity
+    {
+        public IList<Center> centers { get; set; }
+    }
+
+    public class TGSessionsCalendarEntity
+    {
+        public IList<TGCenter> centers { get; set; }
+    }
+
+    public class Center
+    {
+        public int center_id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string state_name { get; set; }
+        public string district_name { get; set; }
+        public string block_name { get; set; }
+        public int pincode { get; set; }
+        public int lat { get; set; }
+        public int _long { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public string fee_type { get; set; }
+        public IList<Session> sessions { get; set; }
+    }
+
+    public class Session
+    {
+        public string session_id { get; set; }
+        public string date { get; set; }
+        public int available_capacity { get; set; }
+        public int min_age_limit { get; set; }
+        public string vaccine { get; set; }
+        public IList<string> slots { get; set; }
+    }
+
+    public class TGCenter
+    {
+        public string name { get; set; }
+        public string fee_type { get; set; }
+        public IList<TGSession> sessions { get; set; }
+    }
+
+    public class TGSession
+    {
+        public string date { get; set; }
+        public int available_capacity { get; set; }
+        public string vaccine { get; set; }
+    }
+
+}
---
+++ CowinAPI/VaccineSlotsChecker.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Telegram.Bot;
+
+namespace CowinAPI
+{
+    public static class VaccineSlotsChecker
+    {
+        [FunctionName("VaccineSlotsChecker")]
+        public static async Task RunAsync([TimerTrigger("%Schedule%")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            int age = int.Parse(GetEnvironmentVariable("age"));
+            string codes = GetEnvironmentVariable("pincodes");
+            List<string> pingcodeList = codes.Split(',').ToList();
+            string dateString = DateTime.Now.ToString("dd-MM-yyyy");
+
+            foreach(var pin in pingcodeList)
+            {
+                await SendSlotsMsgForPincodeAsync(age, dateString, pin, log).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task SendSlotsMsgForPincodeAsync(int age, string dateString, string pin, ILogger log)
+        {
+            string url = $"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pin}&date={dateString}";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            string html = string.Empty;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    html = await reader.ReadToEndAsync().ConfigureAwait(false);
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.Message);
+                return;
+            }            
+
+            SessionsCalendarEntity sessionsEntity = JsonConvert.DeserializeObject<SessionsCalendarEntity>(html);
+            var sessionList = sessionsEntity.centers;
+            var sessions18 = sessionsEntity.centers.Where(x => x.sessions.Any(y => y.min_age_limit == age && y.available_capacity > 0)).ToList();
+            
+            if(sessions18.Count() > 0)
+            {
+                TGSessionsCalendarEntity msgDTO = MapToTGEntity(sessions18);
+                string msg = JsonConvert.SerializeObject(msgDTO, Formatting.Indented);
+
+                await SentTelegramMessageAsync(msg).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task SentTelegramMessageAsync(string msg)
+        {
+            var bot = new TelegramBotClient(GetEnvironmentVariable("botkey"));
+            await bot.SendTextMessageAsync(GetEnvironmentVariable("chatid"), msg).ConfigureAwait(false);
+        }
+
+        private static TGSessionsCalendarEntity MapToTGEntity(IEnumerable<Center> sessions18)
+        {
+            TGSessionsCalendarEntity msgDTO = new TGSessionsCalendarEntity();
+            msgDTO.centers = new List<TGCenter>();
+            foreach (var tgc in sessions18)
+            {
+                var tGSessions = new List<TGSession>();
+                foreach (var ss in tgc.sessions)
+                {
+                    TGSession tGSession = new TGSession
+                    {
+                        available_capacity = ss.available_capacity,
+                        date = ss.date,
+                        vaccine = ss.vaccine
+                    };
+                    if (tGSession.available_capacity > 0)
+                    {
+                        tGSessions.Add(tGSession);
+                    }
+                }
+                TGCenter tGCenter = new TGCenter
+                {
+                    fee_type = tgc.fee_type,
+                    name = tgc.name
+                };
+                tGCenter.sessions = tGSessions;
+                msgDTO.centers.Add(tGCenter);
+            }
+
+            return msgDTO;
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+}
